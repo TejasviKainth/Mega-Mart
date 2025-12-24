7fc8b2391ce968e5b6ccda2225aa45e1dcb64ea9
TejasviKainth <TejasviKainth24@gmail.com>
2025-12-25 00:03:37 +0530

    Dvfbg

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+  origin:'*',
+  credentials:true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
