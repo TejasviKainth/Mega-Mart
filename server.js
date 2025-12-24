8944f5f10d5ff67495bc8f3423b9ce9c4ea55542
TejasviKainth <TejasviKainth24@gmail.com>
2025-12-25 00:09:46 +0530

    Dvfbg

--- server.js
+++ server.js
@@ -18,8 +18,10 @@ const app = express();
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(cors({
-  origin:'https://mega-mart-zeta.vercel.app',
-  credentials:true
+  origin: "https://mega-mart-zeta.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.options("*", cors());
